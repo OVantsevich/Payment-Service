d913c50540af66e3bab38fe9e767bf8a3738b675
olegvantsevich <OVantsevich@gmail.com>
2023-03-07 08:53:31 +0300

    07.03.2023 version 1.12

--- internal/config/mainConfig.go
+++ internal/config/mainConfig.go
@@ -10,11 +10,12 @@ import (
 // MainConfig with init data
 type MainConfig struct {
 	PostgresPort     string `env:"POSTGRES_PORT,notEmpty" envDefault:"5433"`
-	PostgresHost     string `env:"POSTGRES_HOST	,notEmpty" envDefault:"localhost"`
+	PostgresHost     string `env:"POSTGRES_HOST,notEmpty" envDefault:"localhost"`
 	PostgresPassword string `env:"POSTGRES_PASSWORD,notEmpty" envDefault:"postgres"`
 	PostgresUser     string `env:"POSTGRES_USER,notEmpty" envDefault:"postgres"`
 	PostgresDB       string `env:"POSTGRES_DB,notEmpty" envDefault:"postgres"`
 	Port             string `env:"PORT,notEmpty" envDefault:"2000"`
+	Host             string `env:"HOST,notEmpty" envDefault:"localhost"`
 }
 
 // NewMainConfig parsing config from environment
